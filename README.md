b3b0221a0d4829697cc4e21d34bf83433e70ad11
auntfunny <greenauntfunny@gmail.com>
2026-04-13 11:44:37 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Empty React
+
+## 📌 Description
+
+Empty
+
+## 🚀 Deployment
+
+[https://vercel.com/anthony-blacks-projects-a65df513](https://vercel.com/anthony-blacks-projects-a65df513)
+
+## 📁 Repository
+
+[https://github.com/auntfunny](https://github.com/auntfunny)
+
+## ⚙️ Charactaristics
+
+- To be added
+
+## Instructions to Run
---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<h2>Login</h2>} />
+        <Route element={<Outlet />}>
+          <Route path="/" element={<h2>Admin Dashboard</h2>}/>
+          <Route path="/usuarios" element={<h2>Admin Usuarios</h2>}/>
+          <Route path="/categorias" element={<h2>Admin Categorias</h2>}/>
+          <Route path="/paises" element={<h2>Admin Paises</h2>}/>
+          <Route path="/reportes" element={<h2>Admin Reportes</h2>}/>
+        </Route>
+        <Route path="/estudiante"  element={<Outlet />}>
+          <Route path="dash" element={<h2>Student Dashboard</h2>}/>
+          <Route path="reportes" element={<h2>Student Reportes</h2>}/>
+        </Route>
+        <Route element={<Outlet />}>
+          <Route path="/perfil" element={<h2>Perfil</h2>} />
+          <Route path="/cambiarcontraseña" element={<h2>Cambiar Contreseña</h2>} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
