10d10c75886e6b24c81562fb70b82f198acd4688
auntfunny <greenauntfunny@gmail.com>
2026-04-13 17:14:33 -0400

    AuthContext Reviewed

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,27 @@
-import { Route, Routes } from "react-router-dom";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import Login from "./pages/Login";
-import Layout from "./components/Layout";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-
-      {/* LOGIN */}
-      <Route path="/login" element={<Login />} />
-
-      {/* ADMIN */}
-      <Route element={<ProtectedRoute allowedRoles={["ADMIN"]} />}>
-        <Route element={<Layout />}>
-          <Route path="/" element={<h2>Admin Dashboard</h2>} />
-          <Route path="/usuarios" element={<h2>Admin Usuarios</h2>} />
-          <Route path="/categorias" element={<h2>Admin Categorias</h2>} />
-          <Route path="/paises" element={<h2>Admin Paises</h2>} />
-          <Route path="/reportes" element={<h2>Admin Reportes</h2>} />
+    <>
+      <Routes>
+        <Route path="/login" element={<h2>Login</h2>} />
+        <Route element={<Outlet />}>
+          <Route path="/" element={<h2>Admin Dashboard</h2>}/>
+          <Route path="/usuarios" element={<h2>Admin Usuarios</h2>}/>
+          <Route path="/categorias" element={<h2>Admin Categorias</h2>}/>
+          <Route path="/paises" element={<h2>Admin Paises</h2>}/>
+          <Route path="/reportes" element={<h2>Admin Reportes</h2>}/>
         </Route>
-      </Route>
-
-      {/* ESTUDIANTE */}
-      <Route element={<ProtectedRoute allowedRoles={["STUDENT"]} />}>
-        <Route element={<Layout />}>
-          <Route path="/estudiante/dash" element={<h2>Student Dashboard</h2>} />
-          <Route path="/estudiante/reportes" element={<h2>Student Reportes</h2>} />
+        <Route path="/estudiante"  element={<Outlet />}>
+          <Route path="dash" element={<h2>Student Dashboard</h2>}/>
+          <Route path="reportes" element={<h2>Student Reportes</h2>}/>
         </Route>
-      </Route>
-
-      {/* GENERAL */}
-      <Route element={<ProtectedRoute />}>
-        <Route element={<Layout />}>
+        <Route element={<Outlet />}>
           <Route path="/perfil" element={<h2>Perfil</h2>} />
-          <Route path="/cambiarcontraseña" element={<h2>Cambiar Contraseña</h2>} />
+          <Route path="/cambiarcontraseña" element={<h2>Cambiar Contreseña</h2>} />
         </Route>
-      </Route>
-
-    </Routes>
+      </Routes>
+    </>
   );
 }
 
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
@@ -8,6 +9,7 @@ export function AuthProvider({ children }) {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [role, setRole] = useState(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   //Verificar sesión al cargar
   useEffect(() => {
@@ -43,7 +45,6 @@ export function AuthProvider({ children }) {
 
       setRole(payload.role);
       setIsAuthenticated(true);
-
     } catch (error) {
       logout();
     } finally {
@@ -52,60 +53,97 @@ export function AuthProvider({ children }) {
   }, []);
 
   //LOGIN
-  const login = async (data) => {
-    const token = data.access_token;
-
-    if (!token) return;
-
-    localStorage.setItem("token", token);
+  const login = async (email, password) => {
+    setLoading(true);
 
     try {
-      const decoded = jwtDecode(token);
+      const res = await fetch(
+        "https://hs-api.devfunval.cloud/api/v1/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        },
+      );
 
-      //VALIDACIÓN DE ROL
-      const validRoles = ["ADMIN", "STUDENT"];
-
-      if (!validRoles.includes(decoded.role)) {
-        logout();
+      if (!res.ok) {
+        alert("Credenciales incorrectas");
+        setLoading(false);
         return;
       }
 
-      setUser({
-        id: decoded.sub,
-        role: decoded.role,
-      });
+      const data = await res.json();
 
-      setRole(decoded.role);
-      setIsAuthenticated(true);
+      const token = data.access_token;
 
+      if (!token) return;
+
+      localStorage.setItem("token", token);
+
+      try {
+        const decoded = jwtDecode(token);
+
+        //VALIDACIÓN DE ROL
+        const validRoles = ["ADMIN", "STUDENT"];
+
+        if (!validRoles.includes(decoded.role)) {
+          logout();
+          return;
+        }
+
+        setUser({
+          id: decoded.sub,
+          role: decoded.role,
+        });
+
+        setRole(decoded.role);
+        setIsAuthenticated(true);
+      } catch (error) {
+        console.error("Error decodificando token", error);
+        logout();
+      }
+
+      //decodificar token
+      const payload = JSON.parse(atob(data.access_token.split(".")[1]));
+
+      // redirección según rol
+      if (payload.role === "ADMIN") {
+        navigate("/");
+      } else if (payload.role === "STUDENT") {
+        navigate("/estudiante/dash");
+      }
     } catch (error) {
-      console.error("Error decodificando token", error);
-      logout();
+      console.error("Error en login:", error);
+      alert("Error al conectar con el servidor");
+    } finally {
+      setLoading(false);
     }
   };
 
   //LOGOUT
-const logout = async () => {
-  try {
-    await fetch("https://hs-api.devfunval.cloud/api/v1/auth/logout", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-  } catch (error) {
-    console.warn("Error al hacer logout en el servidor");
-  }
+  const logout = async () => {
+    try {
+      await fetch("https://hs-api.devfunval.cloud/api/v1/auth/logout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    } catch (error) {
+      console.warn("Error al hacer logout en el servidor");
+    }
 
-  // limpiar sesión local SIEMPRE
-  localStorage.removeItem("token");
-  setUser(null);
-  setRole(null);
-  setIsAuthenticated(false);
+    // limpiar sesión local SIEMPRE
+    localStorage.removeItem("token");
+    setUser(null);
+    setRole(null);
+    setIsAuthenticated(false);
 
-  //redirección
-  window.location.href = "/#/login";
-};
+    //redirección
+    window.location.href = "/#/login";
+  };
 
   return (
     <AuthContext.Provider
