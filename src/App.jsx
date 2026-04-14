391611bfa51e3cde0fa7ee99209ccf5b14d9c942
Ruben Salazar <rubendariosalazarcamacho@gmail.com>
2026-04-13 20:17:20 -0500

    dashboard admin: muestra datos de usuarios, reportes, categorias y cursos

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Outlet, Route, Routes } from "react-router-dom";
+import DashboardAdmin from "./pages/DashboardAdmin";
 
 function App() {
   return (
@@ -6,7 +7,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<h2>Login</h2>} />
         <Route element={<Outlet />}>
-          <Route path="/" element={<h2>Admin Dashboard</h2>}/>
+          <Route path="/" element={<DashboardAdmin/>}/>
           <Route path="/usuarios" element={<h2>Admin Usuarios</h2>}/>
           <Route path="/categorias" element={<h2>Admin Categorias</h2>}/>
           <Route path="/paises" element={<h2>Admin Paises</h2>}/>
