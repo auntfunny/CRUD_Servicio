270b9fead9632805aeae6d8e9286af2f6441c05e
Ruben Salazar <rubendariosalazarcamacho@gmail.com>
2026-04-15 12:11:11 -0500

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
 import { useAuth } from "../context/AuthContext";
 
 export default function Navbar({ abierto, setAbierto }) {
-  const { user, role, logout } = useAuth();
+  const { user, logout } = useAuth();
+  
+  console.log(user);
+  if (!user) return null;
 
   return (
     <div className="flex justify-around bg-blue-100">
@@ -13,8 +16,10 @@ export default function Navbar({ abierto, setAbierto }) {
           <div className="w-6 h-0.5 bg-black"></div>
         </div>
       </button>
+
+
       <span className="text-red-500">
-        {role} - {user?.first_name}
+        {user && user.first_name}
       </span>
 
       <button className="border-2 bg-gray-200 " onClick={logout}>
--- src/pages/DashboardAdmin.jsx
+++ src/pages/DashboardAdmin.jsx
@@ -1,12 +1,13 @@
 import { useAuth } from "../context/AuthContext";
+import useAxios from "../hooks/useAxios";
 
 
 function DashboardAdmin() {
-  const { user, role } = useAuth();
-  const respuesta = {
-    role: role,
-    userName: user?.id,
-  }; 
+  const { data, loading, error } = useAxios("/dashboard/stats");
+
+
+  if (loading) return <p>Cargando dashboard...</p>;
+  if (error) return <p>Error cargando datos</p>; 
 
   return (
     
@@ -15,21 +16,21 @@ function DashboardAdmin() {
       <section className="p-4">
         <div className="flex flex-col bg-blue-900 text-white font-montserrat gap-2 p-2 rounded-2xl">
           <h2 className="font-avenir text-2xl text-blue-400 font-bold">Usuarios</h2>
-          <p>Total usuarios: {respuesta.users?.total_students + respuesta.users?.total_admins}</p>
-          <p>Estudiantes: {respuesta.users?.total_students}</p>
-          <p>Administradores: {respuesta.users?.total_admins}</p>
+          <p>Total usuarios: {data?.users?.total_students + data?.users?.total_admins}</p>
+          <p>Estudiantes: {data?.users?.total_students}</p>
+          <p>Administradores: {data?.users?.total_admins}</p>
         </div>
       </section>
       <section className="p-4">
         <div className="flex flex-col bg-blue-900 text-white font-montserrat gap-2 p-2 rounded-2xl">
           <h2 className="font-avenir text-2xl text-blue-400 font-bold">Reportes</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 md:text-[15px] gap-2 mt-0.5">
-            <p>Total reportes: {respuesta.reports?.total}</p>
-            <p>Pendientes: {respuesta.reports?.pending}</p>
-            <p>Aprobados: {respuesta.reports?.approved}</p>
-            <p>Rechazados: {respuesta.reports?.rejected}</p>
-            <p>Horas presentadas: {respuesta.reports?.total_hours_submitted}</p>
-            <p>Horas aprobadas: {respuesta.reports?.total_hours_approved}</p>
+            <p>Total reportes: {data?.reports?.total}</p>
+            <p>Pendientes: {data?.reports?.pending}</p>
+            <p>Aprobados: {data?.reports?.approved}</p>
+            <p>Rechazados: {data?.reports?.rejected}</p>
+            <p>Horas presentadas: {data?.reports?.total_hours_submitted}</p>
+            <p>Horas aprobadas: {data?.reports?.total_hours_approved}</p>
           </div>
 
         </div>
@@ -38,7 +39,7 @@ function DashboardAdmin() {
         <div className="flex flex-col bg-blue-900 text-white font-montserrat gap-2 p-2 rounded-2xl lg:px-6">
           <h2 className="font-avenir text-2xl text-blue-400 font-bold">Categorías</h2>
           <div className="grid grid-cols-1 text-xs justify-center gap-1 md:gap-3 md:mt-1">
-            {respuesta.top_categories.map((item) => (
+            {data?.top_categories?.map((item) => (
               <div className="flex flex-col bg-gray-600 rounded-xl p-3" key={item.id}>
                 <p className="text-blue-400 font-montserrat font-bold">{item.name}</p>
                 <p>Total reportes: {item.total_reports}</p>
@@ -54,7 +55,7 @@ function DashboardAdmin() {
         <div className="flex flex-col bg-blue-900 text-white font-montserrat gap-2 p-2 rounded-2xl lg:px-6">
           <h2 className="font-avenir text-2xl text-blue-400 font-bold">Cursos</h2>
           <div className="grid grid-cols-1 text-xs justify-center gap-1 md:grid-cols-2 md:gap-4.5">
-            {respuesta.top_courses.map((item) => (
+            {data?.top_courses?.map((item) => (
               <div className="bg-gray-600 rounded-xl p-4" key={item.id}>
                 <p className="text-blue-400 font-montserrat font-bold">{item.name}</p>
                 <p>Total estudiantes: {item.total_students}</p>
